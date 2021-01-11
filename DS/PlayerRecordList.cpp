daad65691a3ac5cb6fa4d2da0cbb060f7123be93
Marcin <golismarcin@gmail.com>
2021-01-11 22:32:25 +0100

    Add PlayerRecordList class.

--- DS/PlayerRecordList.cpp
+++ DS/PlayerRecordList.cpp
@@ -1,6 +1,6 @@
 #include "PlayerRecordList.h"
 
-PlayerRecordList::PlayerRecordList()
+PlayerRecordList::PlayerRecordList(string name, int score): name(name), score(score)
 {
-    //ctor
+
 }
--- DS/PlayerRecordList.h
+++ DS/PlayerRecordList.h
@@ -1,15 +1,16 @@
 #ifndef PLAYERRECORDLIST_H
 #define PLAYERRECORDLIST_H
+#include <string>
 
-
+using namespace std;
 class PlayerRecordList
 {
     public:
-        PlayerRecordList();
-
-    protected:
+        PlayerRecordList(string name, int score);
 
     private:
+        string name;
+        int score;
 };
 
 #endif // PLAYERRECORDLIST_H
